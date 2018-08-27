b952009433d1735eec8ed1b09efd3346066d43a7
Jonas <jonas.n@t-online.de>
2018-08-27 21:04:50 +0200

    Removed content editable

--- src/components/presentational/Canvas.js
+++ src/components/presentational/Canvas.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Canvas = ({ onMouseDown, endPaintEvent, onMouseMove, updateCanvas }) => (
     <canvas ref={(ref) => (updateCanvas(ref))} 
-        onMouseDown={onMouseDown} onMouseLeave={endPaintEvent} onMouseUp={endPaintEvent} onMouseMove={onMouseMove} 
-        contentEditable="true"/>
+        onMouseDown={onMouseDown} onMouseLeave={endPaintEvent} onMouseUp={endPaintEvent} onMouseMove={onMouseMove} />
 );
 
 export default Canvas;
